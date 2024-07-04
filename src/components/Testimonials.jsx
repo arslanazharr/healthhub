def293ae05fc85a2556ff34ae360f0f6df009b7b
Arslan <arslanazhar00@gmail.com>
2024-07-04 19:25:15 +0500

    added marquee effect to testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-key */
 import { feedbacks } from "../utils/constants";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import Marquee from "react-fast-marquee";
 
 const Testimonials = () => (
   <div className="bg-gray-100 py-10 px-5">
@@ -8,21 +9,23 @@ const Testimonials = () => (
       <p>Smiling</p>
       <p className="font-bold">Faces</p>
     </div>
-    <div className="flex overflow-auto w-full gap-4">
-      {feedbacks.map((feedback, index) => (
-        <div
-          key={index}
-          className="border-[1px] border-main p-4 min-w-[350px] max-w-[351px] flex flex-col gap-2 bg-secondary rounded-xl text-white hover:bg-transparent hover:text-black cursor-default transition"
-        >
-          <div className="flex flex-row text-2xl gap-2">
-            <AccountCircleOutlinedIcon className="!text-4xl" />
-            <h4> {feedback.name}</h4>
-          </div>
+    <Marquee pauseOnHover="true" gradient="true">
+      <div className="flex overflow-auto w-full gap-4">
+        {feedbacks.map((feedback, index) => (
+          <div
+            key={index}
+            className="border-[1px] border-main p-4 min-w-[350px] max-w-[351px] flex flex-col gap-2 bg-secondary rounded-xl text-white hover:bg-transparent hover:text-black cursor-default transition"
+          >
+            <div className="flex flex-row text-2xl gap-2">
+              <AccountCircleOutlinedIcon className="!text-4xl" />
+              <h4> {feedback.name}</h4>
+            </div>
 
-          <div>{feedback.review}</div>
-        </div>
-      ))}
-    </div>
+            <div>{feedback.review}</div>
+          </div>
+        ))}
+      </div>
+    </Marquee>
   </div>
 );
 
